f28bd5a28df3307ad0a8eadcd8e46b28736d8ee8
Maksym Pakula <gargantadoto@gmail.com>
2026-04-06 01:47:36 +0300

    Add all crud functions

---
+++ crud.py
@@ -0,0 +1,51 @@
+from sqlalchemy.orm import Session
+
+import models
+from schemas import AuthorCreate, BookCreate
+
+
+def create_author(db: Session, author: AuthorCreate):
+    db_author = models.Author(
+        name = author.name,
+        bio = author.bio,
+    )
+
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
+
+
+def get_authors_list(
+    db: Session,
+    skip: int = 0,
+    limit: int = 10,
+):
+    return db.query(models.Author).offset(skip).limit(limit).all()
+
+
+def get_author_by_id(db: Session, author_id: int):
+    return db.query(models.Author).filter(models.Author.id == author_id).first()
+
+
+def get_books_list(db: Session, author_id: int, skip: int = 0, limit: int = 10):
+    queryset = db.query(models.Book)
+
+    if author_id is not None:
+        queryset = queryset.filter(models.Book.author_id == author_id)
+
+    return queryset.offset(skip).limit(limit).all()
+
+
+def create_book(db: Session, book: BookCreate):
+    db_book = models.Book(
+        title = book.title,
+        summary = book.summary,
+        publication_date=book.publication_date,
+        author_id = book.author_id,
+    )
+
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
