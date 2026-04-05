f829c0305350ac153c29fd462e94306fb0d210d0
Maksym Pakula <gargantadoto@gmail.com>
2026-04-05 23:30:47 +0300

    Add missed strings length

--- models.py
+++ models.py
@@ -8,8 +8,8 @@ class Author(Base):
     __tablename__ = "author"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
-    bio = Column(String, nullable=False)
+    name = Column(String(255), unique=True)
+    bio = Column(String(512), nullable=False)
     books = relationship("Book", backref="author")
 
 
@@ -17,7 +17,7 @@ class Book(Base):
     __tablename__ = "book"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    summary = Column(String)
+    title = Column(String(255))
+    summary = Column(String(512))
     publication_date = Column(Date)
     author_id = Column(Integer, ForeignKey("author.id"))
