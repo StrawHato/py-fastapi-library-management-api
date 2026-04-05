578230a1d2078a1bba2c406be7496dfc8f9e613b
Maksym Pakula <gargantadoto@gmail.com>
2026-04-06 02:31:19 +0300

    Add author check before creating book and db start on API start

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
 import schemas
-from database import SessionLocal
+import models
+from database import SessionLocal, engine
 
-app = FastAPI()
+
+def create_db_and_tables():
+    models.Base.metadata.create_all(bind=engine)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Connecting to DB")
+    create_db_and_tables()
+
+    yield
+
+    print("Closing DB")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 def get_db():
@@ -39,6 +57,14 @@ def get_author_by_id(author_id: int, db: Session = Depends(get_db)):
 
 @app.post("/books/", response_model=schemas.BooksList)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    author = crud.get_author_by_id(db=db, author_id=book.author_id)
+
+    if not author:
+        raise HTTPException(
+            status_code=404,
+            detail="Author not found"
+        )
+
     return crud.create_book(db=db, book=book)
 
 
